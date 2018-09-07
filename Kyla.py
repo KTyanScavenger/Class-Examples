505f87fc92132460fad5e1817dbbfb1b1484da9e
KTyanScavenger <kylabug@live.com>
2018-09-07 09:30:00 -0600

    Unit 1 Ex.

---
+++ Kyla.py
@@ -0,0 +1,32 @@
+print("this is a string")
+name="Kyla"
+print(type(name))
+fact="I am a comic book addict."
+msg=("My name is " + name + " and " + fact)
+print(msg)
+print("My name is", name, "and", fact)
+
+num=8.0
+num2=14
+num3="50"
+print(num+num2)
+answer=num-num2
+print(answer)
+
+
+
+#5 variables with words.
+print("adjective:  ")
+wd1=input()
+print("common noun ending in 's':   ")
+wd2=input()
+print("proper noun:   ")
+wd3=input()
+print("proper noun:   ")
+wd4=input()
+print("proper noun:   ")
+wd5=input()
+
+print("The Triceratops was a dinosaur that looked " + wd1 + " because it had 3 " + wd2 + " on top of its head. It used these to stay safe from enemies like " + wd3 + ". It was a " + wd4 +
+      " dinosaur, but not the biggest. It was only 10 feet tall. That is not even as tall as 2 " + wd5 + "put together!")
+
