6c2a9ae2be3d359a52509bfd75829e38806b2a3a
KTyanScavenger <kylabug@live.com>
2018-09-13 09:54:38 -0600

    practice definitions

---
+++ Get_Name.py
@@ -0,0 +1,33 @@
+#Kyla Ryan
+#9/18
+#Get Name Function
+def get_name():
+    name=input("What is your name? ")
+
+
+#display name
+    print("The name you entered was", name)
+
+
+
+
+
+print("This is our function")
+get_name()
+
+
+
+def areaOfCircle():
+    PI = 3.14159263
+#1 Get a radius
+    radius = input("What is radius? ")
+    
+    
+#2 compute an area
+    radius = float(radius)
+    area = radius*radius*PI
+#3 Display informaiton back
+    print("The area of the circle is: ",area)
+
+areaOfCircle()
+
