4c53e3e102c8899ad82e9840ca1f85bc35e43f5d
KTyanScavenger <kylabug@live.com>
2018-09-17 10:07:17 -0600

    both ways

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-#Kyla Ryan
-#9/17
-
-def test_average(total):
-    my_list=[]
-    total_score=int(total)
-    count=0
-    while count != total_score:
-        score = float(input("enter a score:"))
-        my_list.append(score)
-        count=count+1
-                            
-    b=sum(my_list)
-    avg=b/total_score
-    print("your avg is",avg)
-
-
-t=input("enter total tests")
-test_average(t)
-
-
-
-
 def my_average():
     t1=int(input("test 1:"))
     t2=int(input("test 2:"))
